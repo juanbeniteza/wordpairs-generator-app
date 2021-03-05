d83843fee954915cab13db8a44dd61fecd147794
Juan Benitez <juan@fleks.info>
2021-03-04 21:46:43 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import './random_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override 
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'My App',
+      theme: ThemeData(primaryColor: Colors.green[600]),
+      home: RandomWords()
+    );
+  }
+}
+
